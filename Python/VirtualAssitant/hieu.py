c08e6904c39e14e9705a78f639d626951ad7c69d
minhlongmt183 <long.voitpro2k@hcmut.edu.vn>
2020-06-08 08:58:07 +0700

    Virtual v1

---
+++ Python/VirtualAssitant/hieu.py
@@ -0,0 +1,10 @@
+you = "hello"
+
+if you == "":
+    robot_brain = "I can't hear you, try again"
+elif you == "today":
+    robot_brain = "thu 6"
+else:
+    robot_brain = "I'm fine thank you and you"
+
+print(robot_brain)
---
+++ Python/VirtualAssitant/noi.py
@@ -0,0 +1,11 @@
+
+## Keyword: Text to speech python
+
+print("noi")
+
+import pyttsx3
+
+robot_brain = "Hello Edisc"
+robot_mouth = pyttsx3.init()
+robot_mouth.say(robot_brain)
+robot_mouth.runAndWait()
